1b786d2f118b8555ab2314ba4662c0ae5cfbdeb6
ZulfanoRp <zulfano67@gmail.com>
2023-01-10 21:58:32 +0700

    add features toTop and download prettier for tailwind css

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.add("flex");
+    toTop.classList.remove("hidden");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
   }
 };
 
@@ -18,3 +23,5 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+//To Top
